b51093abe659bb0310283edea58c86e351880e0b
udayaKavinda <e19155@eng.pdn.ac.lk>
2024-08-21 13:23:44 +1000

    stage 2.99

---
+++ app/src/main/java/com/example/mapsetup/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.mapsetup
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.mapsetup.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private val REQUEST_NOTIFICATION_PERMISSION = 2
+    private lateinit var mapsButton: Button
+    private lateinit var smartShoeButton: Button
+    private lateinit var dataButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mapsButton = findViewById(R.id.maps_button)
+        smartShoeButton = findViewById(R.id.smart_shoe_button)
+        dataButton = findViewById(R.id.data_visalize)
+
+        mapsButton.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+        smartShoeButton.setOnClickListener {
+//            val intent = Intent(this, MapsActivity::class.java)
+//            startActivity(intent)
+        }
+        dataButton.setOnClickListener {
+            val intent = Intent(this, DataVisualize::class.java)
+            startActivity(intent)
+        }
+
+        uiHandler()
+
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestLocationPermission() {
+        if (!isLocationPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    private fun isNotificationPermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true // Permissions are automatically granted on devices running lower versions.
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && !isNotificationPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                REQUEST_NOTIFICATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    uiHandler()
+                } else {
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            REQUEST_NOTIFICATION_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    uiHandler()
+                } else {
+                    Toast.makeText(this, "Notification permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    fun uiHandler(){
+        if (!isLocationPermissionGranted()) {
+            mapsButton.visibility = View.GONE
+            smartShoeButton.visibility = View.GONE
+            dataButton.visibility = View.GONE
+            requestLocationPermission()
+        }
+        if (!isNotificationPermissionGranted()) {
+            mapsButton.visibility = View.GONE
+            smartShoeButton.visibility = View.GONE
+            dataButton.visibility = View.GONE
+            requestNotificationPermission()
+        }
+        if (isLocationPermissionGranted() && isNotificationPermissionGranted()) {
+            mapsButton.visibility = View.VISIBLE
+            smartShoeButton.visibility = View.VISIBLE
+            dataButton.visibility = View.VISIBLE
+        }
+    }
+}
